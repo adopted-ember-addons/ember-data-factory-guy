cf9116532af92cd731f99acd8b0a94404cb7e116
Dan Sudol <dansudol@yahoo.com>
2014-04-16 16:17:59 +0300

    added test for RestAdapter

---
+++ tests/rest_adapter_factory_test.js
@@ -0,0 +1,49 @@
+var testHelper;
+var store;
+
+module('FixtureFactory with RestAdapter', {
+  setup: function() {
+    testHelper = TestHelper.setup(DS.RESTAdapter);
+    store = testHelper.getStore();
+  },
+  teardown: function() {
+    Em.run(function() { testHelper.teardown(); });
+  }
+});
+
+test("supports hasMany associations to fixtures", function() {
+  var p1 = store.makeFixture('project');
+  var p2 = store.makeFixture('project');
+  var user = store.makeFixture('user', {projects: [p1.id, p2.id]})
+
+  equal(user.get('projects.length'),2, "changes hasMany records")
+})
+
+
+test("when hasMany associations used, belongTo parent is assigned", function() {
+  var p1 = store.makeFixture('project');
+  var user = store.makeFixture('user', {projects: [p1.id]})
+
+  equal(!!p1.get('user'), true , "has parent")
+})
+
+module('DS.Store with ActiveModelAdapter', {
+  setup: function() {
+    testHelper = TestHelper.setup(DS.RESTAdapter);
+    store = testHelper.getStore();
+  },
+  teardown: function() {
+    Em.run(function() { testHelper.teardown(); });
+  }
+});
+
+asyncTest("#make builds and creates record", function() {
+  var user = store.makeFixture('user');
+
+  store.find('user', user.id).then ( function(store_user) {
+    deepEqual(store_user.toJSON(), user.toJSON());
+    start()
+  });
+});
+
+
