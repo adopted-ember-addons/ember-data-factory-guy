f36954aab7315bd78d25de9ab40b2560cb9094fe
Dan Sudol <dansudol@yahoo.com>
2014-04-18 12:17:45 +0300

    new store test

---
+++ tests/store_test.js
@@ -0,0 +1,35 @@
+var container;
+
+module('DS.Store #usingFixtureAdapter', {
+  setup: function() {
+    container = new Ember.Container();
+  },
+  teardown: function() {}
+});
+
+var getStore = function(adapter) {
+  container.register("store:main", DS.Store.extend({adapter: adapter}));
+  return container.lookup("store:main");
+}
+
+test("with DS.FixtureAdapter", function() {
+  var adapter = DS.FixtureAdapter
+  equal(getStore(adapter).usingFixtureAdapter(), true );
+});
+
+test("when extending DS.FixtureAdapter", function() {
+  var adapter = DS.FixtureAdapter.extend({});
+  equal(getStore(adapter).usingFixtureAdapter(), true );
+});
+
+test("with DS.RESTAdapter", function() {
+  var adapter = DS.RESTAdapter
+  equal(getStore(adapter).usingFixtureAdapter(), false );
+});
+
+test("with DS.ActiveModelAdapter", function() {
+  var adapter = DS.ActiveModelAdapter
+  equal(getStore(adapter).usingFixtureAdapter(), false );
+});
+
+
