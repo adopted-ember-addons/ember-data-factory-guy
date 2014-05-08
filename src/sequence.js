78e7a831b0e799b65fc14efc56cf6767a91c37e6
Dan Sudol <dansudol@yahoo.com>
2014-05-08 11:57:43 +0300

    minor touch up

--- src/sequence.js
+++ src/sequence.js
@@ -1,6 +1,5 @@
 Sequence = function (fn) {
   var index = 1;
-  var fn = fn;
 
   this.next = function () {
     return fn.call(this, index++);
@@ -9,8 +8,8 @@ Sequence = function (fn) {
   this.reset = function () {
     index = 1;
   }
-}
+};
 
 function MissingSequenceError(message) {
   this.toString = function() { return message }
-}
+};
