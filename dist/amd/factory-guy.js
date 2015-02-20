8dfcfbbb19a84be5979fbc0fddd9cd74a7ee6078
Pedro Kiefer <pedro@kiefer.com.br>
2015-02-20 10:20:39 -0200

    Update build

--- dist/amd/factory-guy.js
+++ dist/amd/factory-guy.js
@@ -649,6 +649,40 @@ var MockCreateRequest = function(url, store, modelName, options) {
   $.mockjax(this.handler);
 };
 
+var MockUpdateRequest = function(url, model, mapFind, options) {
+	var status = options.status || 200;
+	var succeed = options.succeed || true;
+
+	this.andReturn = function(returns) {
+		return this;
+	};
+
+	this.andFail = function(options) {
+		succeed = false;
+		status = options.status || 500;
+		return this;
+	};
+
+	this.handler = function(settings) {
+		if (!succeed) {
+			this.status = status;
+		} else {
+			var json = model.toJSON({includeId: true});
+			this.responseText = mapFind(model.constructor.typeKey, json);
+			this.status = 200;
+		}
+	};
+
+	var requestConfig = {
+		url: url,
+		dataType: 'json',
+		type: 'PUT',
+		status: status,
+		response: this.handler
+	};
+
+	$.mockjax(requestConfig);
+};
 (function () {
   DS.Store.reopen({
     /**
