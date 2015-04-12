090b6c016aa4a1ba225e37d5d7195f42d3b76348
Dan Sudol <dansudol@yahoo.com>
2015-04-12 15:57:04 +0300

    touch up index.js

--- addon/index.js
+++ addon/index.js
@@ -1,6 +1,5 @@
 import FactoryGuy from './factory-guy';
 import { make, makeList, build, buildList, clearStore } from './factory-guy';
 
-//module.exports = { default: FactoryGuy, make: make, makeList: makeList, build: build, buildList: buildList };
 export default FactoryGuy;
 export { make, makeList, build, buildList, clearStore };
