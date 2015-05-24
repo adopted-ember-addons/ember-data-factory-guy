2734506c038fbdb181e8aa7926ed0f8e8ee245ad
Dan Sudol <dansudol@yahoo.com>
2015-05-24 15:07:12 -0400

    adding model test sample with new faster style

---
+++ tests/unit/models/profile-test.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import FactoryGuy, { make } from 'ember-data-factory-guy';
+
+/**
+ * This test actually is NOT using startApp function or App.destroy for cleanup,
+ * so it is alot faster. Ember-Qunit starts an application for you at the beginning of
+ * the test run, so for model tests, you don't need to start it again
+ * (to get your own copy of a new application), just clearing the store at the end
+ * is fine. Every model test can do this and as long as you keep clearing the store
+ * your fine.
+ */
+module('Profile Model', {
+  afterEach: function() {
+    Ember.run(FactoryGuy, 'clearStore');
+  }
+});
+
+test('has company', function() {
+  var profile = make('profile', 'with_company');
+  ok(profile.get('company.profile') === profile);
+});
