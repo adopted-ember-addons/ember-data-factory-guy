3e575ef2c740b32060e35aade4b3d54e361018e2
Dan Sudol <dansudol@yahoo.com>
2015-07-03 08:43:47 -0400

    cleaned up test code

--- tests/acceptance/users-view-test.js
+++ tests/acceptance/users-view-test.js
@@ -2,32 +2,23 @@ import Ember from 'ember';
 
 import { make } from 'ember-data-factory-guy';
 import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
-import { theUsualSetup, theUsualTeardown } from '../helpers/utility-methods';
-
 import startApp from '../helpers/start-app';
 
 var App;
 
 module('Users View', {
-  setup: function () {
-    App = theUsualSetup('-json-api');
-    //App = theUsualSetup('-active-model');
+  beforeEach: function () {
+    Ember.run(function () {
+      App = startApp();
+      TestHelper.setup();
+    });
   },
-  teardown: function () {
-    theUsualTeardown(App);
+  afterEach: function () {
+    Ember.run(function () {
+      TestHelper.teardown();
+      App.destroy();
+    });
   }
-  //beforeEach: function () {
-  //  Ember.run(function () {
-  //    App = startApp();
-  //    TestHelper.setup();
-  //  });
-  //},
-  //afterEach: function () {
-  //  Ember.run(function () {
-  //    TestHelper.teardown();
-  //    App.destroy();
-  //  });
-  //}
 });
 
 
