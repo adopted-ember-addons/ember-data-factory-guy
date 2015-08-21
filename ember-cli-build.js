f95767ada57b507928e9fb7eee90999dacaf01cb
Dan Sudol <dansudol@yahoo.com>
2015-08-21 22:19:04 +0200

    update to ember-cli v1.13.8

---
+++ ember-cli-build.js
@@ -0,0 +1,13 @@
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function (defaults) {
+
+  var app = new EmberAddon(defaults, {
+    // Any other options
+  });
+
+  app.import('bower_components/jquery-mockjax/dist/jquery.mockjax.js');
+  app.import('bower_components/sinon/index.js');
+
+  return app.toTree();
+};
