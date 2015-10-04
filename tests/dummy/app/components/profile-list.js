001caa70a4d82112beacf32707f3f0ecec7975d7
Dan Sudol <dansudol@yahoo.com>
2015-10-03 20:31:42 -0400

    updated component test example

--- tests/dummy/app/components/profile-list.js
+++ tests/dummy/app/components/profile-list.js
@@ -1,10 +1,2 @@
 import Ember from 'ember';
-export default Ember.Component.extend({
-
-  didInsertElement: function() {
-    //console.log(this.get('attrs'))
-    //console.log(this.get('profiles'))
-    //console.log(this.get('attrs.profiles'))
-    //console.log(this.get('profiles.firstObject'))
-  }
-});
+export default Ember.Component.extend({});
--- tests/unit/components/dude-translator-test.js
+++ tests/unit/components/dude-translator-test.js
@@ -1,34 +1,33 @@
 import Ember from 'ember';
 import FactoryGuy, { make, clearStore }  from 'ember-data-factory-guy';
 import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
+import hbs from 'htmlbars-inline-precompile';
 import startApp from '../../helpers/start-app';
 
 import { test, moduleForComponent } from 'ember-qunit';
 
 var App = null;
 
-moduleForComponent('translate', 'Translate', {
-  unit: true,
+moduleForComponent('user-list', {
+  integration: true,
+
   setup: function () {
     Ember.run(function () {
       App = startApp();
-      TestHelper.setup();
     });
   },
 
   teardown: function () {
-    Ember.run(function () {
-      TestHelper.teardown();
-      App.destroy();
-    });
+    Ember.run(App,'destroy');
   }
 });
 
 
 test("can translate original word", function () {
-  var component = this.subject();
   var user = make('user', {name: 'Rob'});
-  component.set('original', user.get('name'));
-  this.render();
-  ok(find('.translation').text() === 'Rob dude');
+
+  this.render(hbs`{{dude-translator original=name}}`);
+  this.set('name', user.get('name'));
+
+  ok(this.$('.translation').text() === 'Rob dude');
 });
