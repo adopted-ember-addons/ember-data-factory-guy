707d961ecbb35436791b0e42b29edb70c553ab49
Anil <anilmaurya8dec@gmail.com>
2015-11-05 01:20:30 +0530

    Fix jquery-mockjax bower package addition to Project

--- blueprints/ember-data-factory-guy/index.js
+++ blueprints/ember-data-factory-guy/index.js
@@ -2,10 +2,8 @@ module.exports = {
   // Hack for previous versions of Ember CLI
   normalizeEntityName: function() {},
 
-  afterInstall: function(options) {
+  afterInstall: function() {
     return this.addBowerPackageToProject('jquery-mockjax', '2.0.1');
-  },
-
-  initialize: function(container, application) {
   }
+
 };
