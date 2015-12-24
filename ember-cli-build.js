6807f5ee63b741b65491d7b12c64266d515e50af
Dan Sudol <dansudol@yahoo.com>
2015-12-24 07:05:37 -0500

    travis fix

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ module.exports = function (defaults) {
   var app = new EmberAddon(defaults, {
     // Any other options
     babel: {
-      //includePolyfill: true
+      includePolyfill: true
     }
   });
 
