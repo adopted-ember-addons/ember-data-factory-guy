198423b97d14c4c5ad1b1a489abc3235a8912e13
Dan Sudol <dansudol@yahoo.com>
2016-05-01 21:27:13 +0200

    rearranged tests for verify mock timesCalled

---
+++ tests/unit/mock-get-test.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+import FactoryGuy, { build, mockFindAll, mockQueryRecord } from 'ember-data-factory-guy';
+import moduleForAcceptance from '../helpers/module-for-acceptance';
+
+moduleForAcceptance('MockGetRequest#timeCalled');
+
+test("can verify how many times a queryRecord call was mocked", function(assert) {
+  Ember.run(()=> {
+    var done = assert.async();
+    const mock = mockQueryRecord('company', {}).returns({ json: build('company') });
+
+    FactoryGuy.store.queryRecord('company', {}).then(()=> {
+      FactoryGuy.store.queryRecord('company', {}).then(()=> {
+        equal(mock.timesCalled, 2);
+        done();
+      });
+    });
+  });
+});
+
+test("can verify how many times a findAll call was mocked", function(assert) {
+  Ember.run(()=> {
+    var done = assert.async();
+    const mock = mockFindAll('company')
+
+    FactoryGuy.store.findAll('company').then(()=> {
+      FactoryGuy.store.findAll('company').then(()=> {
+        equal(mock.timesCalled, 2);
+        done();
+      });
+    });
+  });
+});
