ab7078f89843a5b1f47da4d5f2121b067662aa86
Dan Sudol <dansudol@yahoo.com>
2016-05-16 19:21:13 -0400

    doc updates

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,8 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+var Funnel = require('broccoli-funnel');
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function (defaults) {
+module.exports = function(defaults) {
 
   var app = new EmberAddon(defaults, {
     // Any other options
@@ -8,6 +10,16 @@ module.exports = function (defaults) {
       includePolyfill: true
     }
   });
+  var env = EmberApp.env();
+  console.log('env', env );
+  var factoryDefinitions = new Funnel('tests/factories', {
+    srcDir: '/',
+    include: ['**/*.js'],
+    destDir: '/assets/vendor'
+  });
+  console.log('factoryDefinitions', factoryDefinitions);
+
+  return app.toTree(factoryDefinitions);
 
   app.import('bower_components/jquery-mockjax/dist/jquery.mockjax.js');
   app.import('bower_components/sinon/index.js');
