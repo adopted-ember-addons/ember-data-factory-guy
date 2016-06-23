0357c9ed91f39023e161dceae059cb611c181cd8
Pat O'Callaghan <patocallaghan@gmail.com>
2016-06-23 01:30:18 +0100

    Added failing test for returns in mockCreate

--- tests/dummy/app/components/single-user.js
+++ tests/dummy/app/components/single-user.js
@@ -8,7 +8,10 @@ export default Ember.Component.extend({
     addProject: function (user) {
       let title = this.$('input.project-title').val();
       let store = this.get('controller.store');
-      this.get('store').createRecord('project', {title: title, user: user}).save();
+      this.get('store').createRecord('project', {title: title, user: user}).save().then((record) => {
+        console.log('Project record ID returned in App code:', record.get('id'));
+        record.reload();
+      });
     }
   }
 });
