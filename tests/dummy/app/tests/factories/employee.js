e9c2ad1090b79d67f3ae411b5d2540cac0830bcc
Pat O'Callaghan <patocallaghan@gmail.com>
2016-09-07 18:48:07 +0100

    Renamed employee.name to employee.designation

--- tests/dummy/app/tests/factories/employee.js
+++ tests/dummy/app/tests/factories/employee.js
@@ -2,23 +2,23 @@ import FactoryGuy from 'ember-data-factory-guy';
 
 FactoryGuy.define('employee', {
   default: {
-    name: FactoryGuy.belongsTo('name'),
+    designation: FactoryGuy.belongsTo('name'),
     titles: ['Mr.', 'Dr.'],
     gender: 'Male',
     birthDate: new Date('2016-05-01')
   },
   traits: {
     default_name_setup: {
-      name: {}
+      designation: {}
     },
     jon: {
-      name: {
+      designation: {
         firstName: 'Jon',
         lastName: 'Snow'
       }
     },
     geoffrey: {
-      name: FactoryGuy.belongsTo('employee_geoffrey')
+      designation: FactoryGuy.belongsTo('employee_geoffrey')
     },
     with_department_employments: {
       departmentEmployments: FactoryGuy.hasMany('department-employment', 2)
