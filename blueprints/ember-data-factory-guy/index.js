479b1a5a2bf57d32f3e2633f9f28dd2ca57b4516
Matt Layman <matthewlayman@gmail.com>
2017-04-06 08:25:57 -0400

    Move jquery-mockjax dependency from bower to npm.

--- blueprints/ember-data-factory-guy/index.js
+++ blueprints/ember-data-factory-guy/index.js
@@ -1,9 +1,4 @@
 module.exports = {
   // Hack for previous versions of Ember CLI
   normalizeEntityName: function() {},
-
-  afterInstall: function() {
-    return this.addPackageToProject('jquery-mockjax', '2.2.1');
-  }
-
 };
