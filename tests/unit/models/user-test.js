96edb977202308ff5ec91681fa2f2dc6ae16516e
Dan Sudol <dansudol@yahoo.com>
2017-05-24 20:39:04 -0400

    cleanup old comments

--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -2,20 +2,6 @@ import Ember from 'ember';
 import {test, moduleForModel} from 'ember-qunit';
 import FactoryGuy, {make, manualSetup, mockFindRecord, mockSetup, mockTeardown}  from 'ember-data-factory-guy';
 
-/**
- This test is NOT using startApp function or App.destroy for cleanup,
- so it is alot faster.
-
- But you do need to user moduleForModel instead of just module
-
- As long as you're using ember 2.3 of have ember-getowner-pollyfill addon installed
- this style should work for you, and be speedier than calling startApp()
-
- If this does not work for you ( for whatever reason ) take a look at
- profile-test.js in this same directory for the fool proof way to write a
- model test.
- */
-
 moduleForModel('user', 'Unit | Model | user', {
   // using integration: true to have all models registered in the container
   // but you could also use needs: [model:project, model:client, etc..] to list those you need as well
