41fb1189700e531c632ab625393add9ac6bd93fc
Dan Sudol <dansudol@yahoo.com>
2018-02-18 13:00:35 -0500

    clean up test-helper

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,6 @@
 import Application from '../app';
 import { setApplication } from '@ember/test-helpers';
-//import resolver from './helpers/resolver';
-//import { setResolver } from '@ember/test-helpers';
 import { start } from 'ember-cli-qunit';
 
 setApplication(Application.create({autoboot: false, rootElement: '#ember-testing'}));
-//setResolver(resolver);
 start();
