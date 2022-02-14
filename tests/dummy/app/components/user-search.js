c48e7a37a6e7ed09baa3f4b5879ea089d53db28f
Pat O'Callaghan <pat@intercom.io>
2022-02-14 23:40:08 +0000

    Fix `this-property-fallback` deprecations (#462)

--- tests/dummy/app/components/user-search.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-
-export default Component.extend({
-  actions: {
-    findUsers(userName) {
-      this.userSearch(userName);
-    },
-  },
-});
--- tests/dummy/app/controllers/search.js
+++ tests/dummy/app/controllers/search.js
@@ -1,11 +1,11 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
-export default Controller.extend({
-  router: service(),
-  actions: {
-    userSearch(name) {
-      this.router.transitionTo('search.results', name);
-    },
-  },
-});
+export default class SearchController extends Controller {
+  @service router;
+  @action
+  userSearch(name) {
+    this.router.transitionTo('search.results', name);
+  }
+}
