f750a3cda36e2faf5417d8782ac0559ea322632d
Pat O'Callaghan <pat@intercom.io>
2022-02-14 21:43:26 +0000

    Fix deprecation workflow

---
+++ tests/dummy/config/deprecation-workflow.js
@@ -0,0 +1,22 @@
+'use strict';
+/* global self */
+
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: 'silence', matchId: 'deprecate-fetch-ember-data-support' },
+    {
+      handler: 'silence',
+      matchId: 'deprecated-run-loop-and-computed-dot-access',
+    },
+    {
+      handler: 'silence',
+      matchId: 'ember.built-in-components.legacy-arguments',
+    },
+    { handler: 'silence', matchId: 'ember-source.deprecation-without-for' },
+    { handler: 'silence', matchId: 'ember-source.deprecation-without-since' },
+    { handler: 'silence', matchId: 'implicit-injections' },
+    { handler: 'silence', matchId: 'routing.transition-methods' },
+    { handler: 'silence', matchId: 'this-property-fallback' },
+  ],
+};
