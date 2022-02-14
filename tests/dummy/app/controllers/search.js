7d67845524eebfb22b7c6cf80bb97e4cc5d72307
Pat O'Callaghan <pat@intercom.io>
2022-02-14 23:27:38 +0000

    Fix a bunch of deprecations (#461)

--- tests/dummy/app/controllers/search.js
+++ tests/dummy/app/controllers/search.js
@@ -1,9 +1,11 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  router: service(),
   actions: {
     userSearch(name) {
-      this.transitionToRoute('search.results', name);
+      this.router.transitionTo('search.results', name);
     },
   },
 });
---
+++ tests/dummy/config/deprecation-workflow.js
@@ -0,0 +1,16 @@
+'use strict';
+/* global self */
+
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: 'silence', matchId: 'deprecate-fetch-ember-data-support' },
+    {
+      handler: 'silence',
+      matchId: 'deprecated-run-loop-and-computed-dot-access',
+    },
+    { handler: 'silence', matchId: 'ember-source.deprecation-without-for' },
+    { handler: 'silence', matchId: 'ember-source.deprecation-without-since' },
+    { handler: 'silence', matchId: 'this-property-fallback' },
+  ],
+};
