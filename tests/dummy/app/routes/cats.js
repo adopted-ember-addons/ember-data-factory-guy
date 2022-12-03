6f109264803b94eb2bfce0bb059c7bf2ac7d784d
Brad Overton <bradley.overton@3plearning.com>
2022-12-03 01:32:11 +0000

    refactor: more octane updates

--- tests/dummy/app/routes/cats.js
+++ tests/dummy/app/routes/cats.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-  model: function () {
+export default class extends Route {
+  model() {
     return this.store.findAll('cat');
-  },
-});
+  }
+}
--- tests/dummy/app/routes/search/results.js
+++ tests/dummy/app/routes/search/results.js
@@ -1,14 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
+export default class extends Route {
   model(params) {
     return this.store.query('user', { name: params.name }).catch((e) => e);
-  },
-
-  setupController(controller, users) {
-    let { meta = {} } = users;
-    controller.set('model', users);
-    controller.set('previousPage', meta.previous);
-    controller.set('nextPage', meta.next);
-  },
-});
+  }
+}
