484528eb0e80083c7b6ed20b8f74a235e5782fca
Jacob Jewell <jacobjewell@eflexsystems.com>
2024-07-11 16:10:42 -0400

    store inject deprecation

--- tests/dummy/app/routes/cats.js
+++ tests/dummy/app/routes/cats.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class extends Route {
+  @service store;
+
   model() {
     return this.store.findAll('cat');
   }
