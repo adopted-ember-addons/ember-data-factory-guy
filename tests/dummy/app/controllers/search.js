b25c6b0aab5bd9e39649dbf27601c31f47e93cfe
Jacob Jewell <jacobjewell@eflexsystems.com>
2024-07-18 13:16:05 -0400

    lint fixes

--- tests/dummy/app/controllers/search.js
+++ tests/dummy/app/controllers/search.js
@@ -1,11 +1,19 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class SearchController extends Controller {
   @service router;
+  @tracked userName;
+
   @action
-  userSearch(name) {
+  onUserSearch(name) {
     this.router.transitionTo('search.results', name);
+  }
+
+  @action
+  onSetUserName(event) {
+    this.userName = event.target.value;
   }
 }
