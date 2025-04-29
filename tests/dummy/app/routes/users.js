ea4c45a169a322f0b28f78f029dd5b56937bdfdf
Brad Overton <bradley.overton@3plearning.com>
2025-04-29 18:11:26 +1000

    chore: drop support for ember-django-adapter

--- tests/dummy/app/routes/users.js
+++ tests/dummy/app/routes/users.js
@@ -1,7 +1,9 @@
 import Route from '@ember/routing/route';
+import { waitForPromise } from '@ember/test-waiters';
 
 export default class extends Route {
   model() {
-    return this.store.findAll('user');
+    // reload: true to ensure we have a promise to wait on before rendering page
+    return waitForPromise(this.store.findAll('user', { reload: true }));
   }
 }
