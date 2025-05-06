708c85dd87403c5aefb2c3a01a06875278bbb922
Brad Overton <bradley.overton@3plearning.com>
2025-05-06 15:39:45 +1000

    chore: cleanup template lint config

--- test-app/.template-lintrc.js
+++ test-app/.template-lintrc.js
@@ -1,10 +1,6 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane',
-  rules: {
-    'no-implicit-this': false,
-    'no-curly-component-invocation': false,
-    'no-action': false,
-  },
+  plugins: [],
+  extends: ['recommended'],
 };
