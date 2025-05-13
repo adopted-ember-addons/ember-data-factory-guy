abe4a12a5f1ff98eada3c1cb30ac0f8f6ae3b32f
Brad Overton <bradley.overton@3plearning.com>
2025-05-13 14:19:19 +1000

    test: implement ember-qunit v9 test-helper changes

--- test-app/tests/test-helper.js
+++ test-app/tests/test-helper.js
@@ -3,7 +3,8 @@ import config from 'test-app/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
 import { installSinon } from 'test-app/tests/helpers/install-sinon';
 
 import 'test-app/tests/factories';
@@ -14,8 +15,8 @@ setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
 
-// TODO add loadTests() call - see ember-qunit v9
-
+setupEmberOnerrorValidation();
+loadTests();
 start({
   setupTestIsolationValidation: true,
 });
