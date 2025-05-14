828af60ebe8eafbb8b93d9bdd365797750710989
Brad Overton <bradley.overton@3plearning.com>
2025-05-14 17:18:31 +1000

    chore: ember-try update

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -15,7 +15,6 @@ module.exports = async function () {
             'ember-source': '~5.12',
             'ember-load-initializers': '^3.0.0', // v3 needed for ember 5+
             'ember-data': '~5.3', // couldnt get earlier versions to work with ember-source 5.12
-            'ember-data-model-fragments': null, // does not support ED > 4.6
             'ember-inflector': '^6.0.0', // higher ED version needs this
           },
         },
@@ -27,7 +26,6 @@ module.exports = async function () {
             'ember-source': '~6.4',
             'ember-load-initializers': '^3.0.0',
             'ember-data': '~5.3', // earliest version that supports ember 6
-            'ember-data-model-fragments': null,
             'ember-inflector': '^6.0.0',
           },
         },
@@ -39,7 +37,6 @@ module.exports = async function () {
             'ember-source': await getChannelURL('release'),
             'ember-load-initializers': '^3.0.0',
             'ember-data': '~5.3',
-            'ember-data-model-fragments': null,
             'ember-inflector': '^6.0.0',
           },
         },
